b7288d2868f82b52160d3dfeef6aeabdfba5f09b
Mr. Rafiqul Islam <rafiqulislamemon05@gmail.com>
2025-07-14 00:21:41 +0600

    Feat : Create Geolocation api in Home Page

--- app/page.js
+++ app/page.js
@@ -1,9 +1,5 @@
-
+import LocationDetector from "@/components/LocationDetector";
 
 export default function Home() {
-  return (
-    <div>
-      <h1 className="text-center text-3xl text-gray-900 uppercase">welcome to next js app</h1>
-    </div>
-  );
+  return <LocationDetector />;
 }
---
+++ components/LocationDetector.jsx
@@ -0,0 +1,44 @@
+"use client";
+import { useState, useEffect } from "react";
+import { useRouter, usePathname, useSearchParams } from "next/navigation";
+import Image from "next/image";
+
+const LocationDetector = () => {
+  const [loading, setLoading] = useState(false);
+  const router = useRouter();
+  const pathName = usePathname();
+  const searchParams = useSearchParams();
+
+  useEffect(() => {
+    setLoading(true);
+    const params = new URLSearchParams(searchParams);
+
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition((position) => {
+        params.set("latitude", position.coords.latitude);
+        params.set("longitude", position.coords.longitude);
+        setLoading(false);
+        router.push(`/current?${params.toString()}`);
+      });
+    }
+  }, [pathName, searchParams]);
+
+  return (
+    <div className="flex flex-col justify-center items-center h-screen bg-slate-700 text-white">
+      {loading && (
+        <>
+          <Image
+            src={"/network.gif"}
+            alt="loading..."
+            width={500}
+            height={500}
+          />
+
+          <p className="text-4xl text-center">Detecting Location...</p>
+        </>
+      )}
+    </div>
+  );
+};
+
+export default LocationDetector;
